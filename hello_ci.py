23252f9682df804fb50f433a1c7ea93489857ac0
wrmichaelmak <wrmm98@gmail.com>
2020-12-22 12:59:59 +0000

    Bad CI test.

--- hello_ci.py
+++ hello_ci.py
@@ -1,30 +1,28 @@
 import json
-import sys
 from os import getenv, path
 from pprint import pprint
+import sys
 
-import click  # pylint: disable=import-error
-import requests  # pylint: disable=import-error
-from dotenv import load_dotenv  # pylint: disable=import-error
+import click # pylint: disable=import-error
+from dotenv import load_dotenv # pylint: disable=import-error
+import requests # pylint: disable=import-error
 
 env = load_dotenv()
-api_url = getenv("API_URL", default="https://api.github.com/graphql")
-github_token = getenv("GITHUB_TOKEN", default=None)
+api_url = getenv(API_URL, default='https://api.github.com/graphql' )
+github_token = getenv("GITHUB_TOKEN",
+default=None)
 
-if github_token is None:
-    sys.exit(
-        "GitHub Token is not set."
-        + "Please set the GITHUB_TOKEN env variable in your system or "
-        + "the .env file of your project."
-    )
+if github_token is None
+    sys.exit("GitHub Token is not set." +
+        "Please set the GITHUB_TOKEN env variable in your system or " +
+        "the .env file of your project.")
 
-client_id = getenv("CLIENT_ID", default="copy_labels.py")
+client_id = getenv(CLIENT_ID, default='copy_labels.py')
 headers = {
-    "Authorization": "bearer {github_token}".format(github_token=github_token),
-    "Accept": "application/vnd.github.bane-preview+json",
-    "Content-Type": "application/json",
+    'Authorization': 'bearer {github_token}'.format(github_token=github_token),
+    'Accept': 'application/vnd.github.bane-preview+json'
+    'Content-Type': 'application/json'
 }
-
 
 def create_label(repo_id, label):
     """
@@ -41,21 +39,18 @@ def create_label(repo_id, label):
             "color": label["color"],
             "description": label["description"],
             "name": label["name"],
-            "repositoryId": repo_id,
+            "repositoryId": repo_id
         }
     }
 
-    with open(
-        path.join(path.dirname(__file__), "queries/create_label.gql"), "r"
-    ) as query_file:
+    with open(path.join(path.dirname(__file__), 'queries/create_label.gql'), 'r') as query_file:
         query = "".join(query_file.readlines())
 
     payload = {"query": query, "variables": query_variables}
     response = requests.post(api_url, data=json.dumps(payload), headers=headers).json()
-    print("Created label {label}".format(label=label["name"]))
+    print('Created label {label}'.format(label=label["name"]))
 
     return response
-
 
 def get_labels(owner, repo):
     """
@@ -67,14 +62,9 @@ def get_labels(owner, repo):
     :return: A tuple with the GitHub id for the repository and a list of labels defined in the repository
     """
 
-    query_variables = {
-        "owner": owner,
-        "name": repo,
-    }
+    query_variables = { "owner": owner, "name": repo, }
 
-    with open(
-        path.join(path.dirname(__file__), "queries/get_repo_data.gql"), "r"
-    ) as query_file:
+    with open(path.join(path.dirname(__file__), 'queries/get_repo_data.gql'), 'r') as query_file:
         query = "".join(query_file.readlines())
 
     payload = {"query": query, "variables": query_variables}
@@ -90,11 +80,8 @@ def get_labels(owner, repo):
         return repo_id, labels
     else:
         raise Exception(
-            "[ERROR] getting issue labels. Status Code: {status_code} - Message: {result}".format(
-                status_code=status_code, result=result["message"]
-            )
-        )
-
+            '[ERROR] getting issue labels. Status Code: {status_code} - Message: {result}'.format(
+                status_code=status_code, result=result["message"]))
 
 def delete_label(label_id):
     """
@@ -105,12 +92,13 @@ def delete_label(label_id):
     """
 
     query_variables = {
-        "deleteLabelInput": {"clientMutationId": client_id, "id": label_id}
+        "deleteLabelInput": {
+            "clientMutationId": client_id,
+            "id": label_id,
+        }
     }
 
-    with open(
-        path.join(path.dirname(__file__), "queries/delete_label.gql"), "r"
-    ) as query_file:
+    with open(path.join(path.dirname(__file__), 'queries/delete_label.gql'), 'r') as query_file:
         query = "".join(query_file.readlines())
 
     payload = {"query": query, "variables": query_variables}
@@ -118,11 +106,10 @@ def delete_label(label_id):
 
     return result
 
-
 @click.command()
-@click.option("--dry", is_flag=True)
-@click.argument("source_repo")
-@click.argument("target_repo")
+@click.option('--dry', is_flag=True)
+@click.argument('source_repo')
+@click.argument('target_repo')
 def copy_labels(source_repo, target_repo, dry):
     """
     Copy labels from the source repository to the target repository.
@@ -137,58 +124,32 @@ def copy_labels(source_repo, target_repo, dry):
     target_owner, target_repo_name = target_repo.split("/")
 
     try:
-        print(
-            "Fetching labels for {source_repo_name} repo.".format(
-                source_repo_name=source_repo_name
-            )
-        )
+        print('Fetching labels for {source_repo_name} repo.'.format(source_repo_name=source_repo_name))
         _, source_repo_labels = get_labels(source_owner, source_repo_name)
-        print(
-            "Fetched labels for {source_repo_name}".format(
-                source_repo_name=source_repo_name
-            )
-        )
+        print('Fetched labels for {source_repo_name}'.format(source_repo_name=source_repo_name))
 
-        print(
-            "Fetching labels for {target_repo_name} repo.".format(
-                target_repo_name=target_repo_name
-            )
-        )
+        print('Fetching labels for {target_repo_name} repo.'.format(target_repo_name=target_repo_name))
         target_repo_id, target_repo_labels = get_labels(target_owner, target_repo_name)
-        print(
-            "Fetched labels for {target_repo_name}".format(
-                target_repo_name=target_repo_name
-            )
-        )
+        print('Fetched labels for {target_repo_name}'.format(target_repo_name=target_repo_name))
 
-        filtered_labels = list(
-            filter(lambda x: x not in target_repo_labels, source_repo_labels)
-        )
+        filtered_labels = list(filter(lambda x: x not in target_repo_labels, source_repo_labels))
 
         if dry:
-            print("This is just a dry run. No labels will be copied/created.")
-            print(
-                "{label_count} labels would have been created.".format(
-                    label_count=len(filtered_labels)
-                )
-            )
+            print('This is just a dry run. No labels will be copied/created.')
+            print('{label_count} labels would have been created.'.format(label_count=len(filtered_labels)))
             pprint(filtered_labels, indent=4)
         else:
-            print(
-                "Preparing to created {label_count} labels in {target_repo}".format(
-                    label_count=len(filtered_labels), target_repo=target_repo
-                )
-            )
+            print('Preparing to created {label_count} labels in {target_repo}'.format(
+                label_count=len(filtered_labels), target_repo=target_repo))
 
             for label in filtered_labels:
                 create_label(target_repo_id, label)
     except Exception as error:
         sys.exit(error)
 
-    print("Done")
-
+    print('Done')
 
 if __name__ == "__main__":
     # Pylint doesn't know that @click.command takes care of injecting the
     # function parameters. Disabling Pylint error.
-    copy_labels()  # pylint: disable=no-value-for-parameter
+    copy_labels() # pylint: disable=no-value-for-parameter
